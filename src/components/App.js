f837c8521a84b57b0db9b7212ffc30bcb5a3960b
Reshav ji <116823969+Reshavji@users.noreply.github.com>
2023-04-19 16:44:14 +0530

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../styles/App.css';
+import styles from '../styles/appStyles.module.css';
+const App = () => {
+const heading ={
+
+
+}
+  return (
+    <div id="main">
+      <h1 >Success</h1>
+      <h1 id='error' >Error</h1>
+    </div>
+  )
+}
+
+
+export default App;
